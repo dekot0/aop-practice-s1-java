0205a3c549d42f1295a3fc0e9a07679b9e8ab018
Коровченко-Топузов <dionisi787@gmail.com>
2026-09-21 22:17:19 +0200

    add question

--- src/main/java/homework/h01/h01.java
+++ src/main/java/homework/h01/h01.java
@@ -1,15 +1,14 @@
 package homework.h01;
 
 public class h01 {
-        public int countOdds(int low, int high) {
+  public int countOdds(int low, int high) {
 
-            int count = (high - low) / 2;
+    int count = (high - low) / 2;
 
+    if (low % 2 != 0 || high % 2 != 0) {
+      count++;
+    }
 
-            if (low % 2 != 0 || high % 2 != 0) {
-                count++;
-            }
-
-            return count;
-        }
-   }
+    return count;
+  }
+}
